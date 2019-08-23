468bcb4aeac94941bb3c6782c71e15fe460a79cc
Lucas Leal <lealadiala@gmail.com>
2019-08-23 08:44:49 -0300

    Change database settings to heroku

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'postgres',
-  host: '127.0.0.1',
-  username: 'docker',
-  password: 'docker',
-  database: 'gonodemodulo2',
+  host: 'ec2-54-221-212-126.compute-1.amazonaws.com',
+  username: 'xvikhhgzewocsa',
+  password: '37f72362cac5fc0939f7767ac3933d39263f1ac0a9e3dc8496e756fd434a3f48',
+  database: 'd4ipkcp73g50hn',
   operatorAliases: false,
   define: {
     timestamps: true,
