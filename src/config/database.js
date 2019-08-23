0652e576395cfdbbf0637a142c252b84fc6c8572
Lucas Leal <lealadiala@gmail.com>
2019-08-23 09:13:51 -0300

    Heroku Commit

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'ec2-54-221-212-126.compute-1.amazonaws.com',
+  host: 'https://ec2-54-221-212-126.compute-1.amazonaws.com',
   username: 'xvikhhgzewocsa',
   password: '37f72362cac5fc0939f7767ac3933d39263f1ac0a9e3dc8496e756fd434a3f48',
   database: 'd4ipkcp73g50hn',
